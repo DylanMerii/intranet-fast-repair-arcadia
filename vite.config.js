8f5f58eb14737904b7880cd9b22dc8af0321f3ac
DylanMerii <dylanloze26@outlook.fr>
2026-04-10 19:05:23 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+base: '/intranet-fast-repair-arcadia/'
+
 // https://vite.dev/config/
 export default defineConfig({
   logLevel: 'error', // Suppress warnings, only show errors
